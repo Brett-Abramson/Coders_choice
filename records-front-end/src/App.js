bc8d5232f2df70c1bbd00061a2c22604a9efdbc9
Steven Gebhardt <stevengebhardt@stevens-mbp.lan>
2023-03-27 15:43:39 -0400

    create route created, add.js create form complete-possibly, app.js handleCreate and get routes established

--- records-front-end/src/App.js
+++ records-front-end/src/App.js
@@ -1,14 +1,33 @@
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Add from "./components/add";
 
 const App = () => {
+  const [records, setRecords] = useState([]);
+  const getRecords = () => {
+    axios.get("http://localhost:3000/records").then((response) => {
+      setRecords(response.data);
+    });
+  };
+  const handleCreate = (data) => {
+    axios.post("http://localhost:3000/records", data).then((response) => {
+      console.log(response);
+      let newRecords = [...records, response.data];
+      setRecords(newRecords);
+    });
+  };
 
+  useEffect(() => {
+    getRecords();
+  }, []);
 
   return (
     <>
       <h1> Record Collection</h1>
+      <Add handleCreate={handleCreate} />
     </>
-
-  )
-}
+  );
+};
 
 export default App;
