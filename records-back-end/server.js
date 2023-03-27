409c2fe1a3ec97c4f69bed650cb11fcb79833ca5
Brett Abramson <brett.m.abramson@gmail.com>
2023-03-27 14:02:06 -0500

    added update route to server.js

--- records-back-end/server.js
+++ records-back-end/server.js
@@ -6,6 +6,17 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.put("/record/:id", (req,res) => {
+  Record.findByIdAndRemove(req.params.id)
+  .then((deletedRecord) => {
+    res.json(deletedRecord)
+  })
+})
+
+
+
+
+
 app.listen(PORT, () => {
   console.log("Hello Seattle, I'm listening...");
 });
